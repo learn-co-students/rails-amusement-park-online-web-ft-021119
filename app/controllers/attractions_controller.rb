b8de8e632c19df41fd1cbab8e1946ccbb5f5a630
Lizzie <lchan217@gmail.com>
2019-05-10 18:20:09 -0500

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -9,6 +9,28 @@ class AttractionsController < ApplicationController
     @attraction = Attraction.new
   end
   def create
-    
+    @attraction = Attraction.create(attraction_params)
+    if @attraction.valid?
+      @attraction.save
+      redirect_to attraction_path(@attraction)
+    else
+      render :new
+    end
+  end
+  def edit
+    @attraction = Attraction.find(params[:id])
+  end
+  def update
+    @attraction = Attraction.find(params[:id])
+    if @attraction.update(attraction_params)
+        redirect_to attraction_path(@attraction)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def attraction_params
+    params.require(:attraction).permit(:name, :min_height, :happiness_rating, :nausea_rating, :tickets)
   end
 end
--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -4,12 +4,11 @@ class RidesController < ApplicationController
     @attraction = Attraction.find_by(id: params[:attraction_id])
 
     @ride = Ride.new(user_id: @user.id, attraction_id: @attraction.id )
-    if @ride.save
-      flash[:alert] = @ride.take_ride
-      redirect_to user_path(@user)
 
-      # @message = @ride.take_ride
-      # redirect_to user_path(@ride.user, :message => @message)
+    if @ride.save
+      @notice = @ride.take_ride
+      redirect_to user_path(@user), notice: @notice
+      #flash[:notice] did not work because it wasn't on same line, buried in controller instead of view
     else
       redirect_to attraction_path(@attraction)
     end
